bd664659ef5fa1f10019fd4ee34129d303914f0a
Christian Alfoni <christianalfoni@gmail.com>
2017-12-07 19:05:46 +0100

    Fixed loading sandbox, code change and forking

--- packages/app/src/app/pages/Sandbox/index.js
+++ packages/app/src/app/pages/Sandbox/index.js
@@ -61,6 +61,9 @@ class SandboxPage extends React.PureComponent<Props, State> {
   }
 
   fetchSandbox = (id: string) => {
+    this.props.signals.editor.sandboxChanged({
+      id: this.props.match.params.id,
+    });
     this.setState({ loading: true }, () => {
       this.props.signals.editor.sandboxChanged({
         id: this.props.match.params.id,
