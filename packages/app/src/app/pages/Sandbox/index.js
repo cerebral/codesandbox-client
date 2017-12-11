6dab13248a6e4eb1cef33cf99ff17e8a98607139
Christian Alfoni <christianalfoni@gmail.com>
2017-12-11 15:35:02 +0100

    workspace, editor and preview running with mobx

--- packages/app/src/app/pages/Sandbox/index.js
+++ packages/app/src/app/pages/Sandbox/index.js
@@ -1,20 +1,8 @@
-// @flow
 import * as React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { inject } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import { Link } from 'react-router-dom';
-import { createSelector } from 'reselect';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
-
-import type { Sandbox } from 'common/types';
-
-import { sandboxesSelector } from 'app/store/entities/sandboxes/selectors';
-import sandboxActionCreators from 'app/store/entities/sandboxes/actions';
-import viewActionCreators from 'app/store/view/actions';
-import { preferencesSelector } from 'app/store/preferences/selectors';
-import { workspaceHiddenSelector } from 'app/store/view/selectors';
 import KeybindingManager from 'app/containers/KeybindingManager';
 
 import Title from 'app/components/text/Title';
@@ -24,35 +12,7 @@ import Centered from 'common/components/flex/Centered';
 import Editor from './Editor';
 import Skeleton from './Editor/Content/Skeleton';
 
-type Props = {
-  sandboxes: { [id: string]: Sandbox },
-  sandboxActions: typeof sandboxActionCreators,
-  viewActions: typeof viewActionCreators,
-  workspaceHidden: boolean,
-  match: { params: { id: string } },
-  zenMode: boolean,
-};
-type State = {
-  notFound: boolean,
-  error: ?string,
-  currentId: ?string,
-};
-
-const mapStateToProps = createSelector(
-  sandboxesSelector,
-  preferencesSelector,
-  workspaceHiddenSelector,
-  (sandboxes, preferences, workspaceHidden) => ({
-    sandboxes,
-    zenMode: preferences.zenMode,
-    workspaceHidden,
-  })
-);
-const mapDispatchToProps = dispatch => ({
-  sandboxActions: bindActionCreators(sandboxActionCreators, dispatch),
-  viewActions: bindActionCreators(viewActionCreators, dispatch),
-});
-class SandboxPage extends React.PureComponent<Props, State> {
+class SandboxPage extends React.Component {
   componentDidMount() {
     if (
       window.screen.availWidth < 800 &&
@@ -64,65 +24,29 @@ class SandboxPage extends React.PureComponent<Props, State> {
         addedSign +
         'codemirror=1';
     } else {
-      this.fetchSandbox(this.props.match.params.id);
+      this.fetchSandbox();
     }
   }
 
-  fetchSandbox = (id: string) => {
-    this.setState({ loading: true }, () => {
+  fetchSandbox = () => {
+    // Hack until new router in place, have to get user first
+    setTimeout(() => {
       this.props.signals.editor.sandboxChanged({
         id: this.props.match.params.id,
       });
-      this.props.sandboxActions
-        .getById(id)
-        .then(this.setId, this.handleNotFound);
-    });
+    }, 100);
   };
 
-  setId = (sandbox: Sandbox) => {
-    this.setState({ currentId: sandbox.id, loading: false });
-  };
-
-  componentDidUpdate(oldProps) {
-    const newId = this.props.match.params.id;
-    const oldId = oldProps.match.params.id;
-
-    if (newId != null && oldId != null && oldId !== newId) {
-      this.props.signals.editor.sandboxChanged({
-        id: this.props.match.params.id,
-      });
-      const sandbox = this.props.sandboxes[newId];
-      this.setState({ notFound: false }); // eslint-disable-line
-      if (!sandbox || !sandbox.forked) {
-        this.fetchSandbox(newId);
-      } else {
-        // Sandbox is already in state, so change currentId to this one
-        this.setId(sandbox);
-      }
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.id !== this.props.match.params.id) {
+      this.fetchSandbox();
     }
   }
-
-  handleNotFound = e => {
-    if (e.response && e.response.status === 404) {
-      this.setState({ notFound: true, loading: false });
-    } else {
-      this.setState({ error: e.apiMessage || e.message, loading: false });
-    }
-  };
-
-  state = { notFound: false, currentId: null, loading: false };
 
   render() {
-    const {
-      sandboxes,
-      match,
-      zenMode,
-      workspaceHidden,
-      viewActions,
-    } = this.props;
-    const { currentId } = this.state;
+    const { match, store } = this.props;
 
-    if (this.state.loading) {
+    if (store.editor.isLoading) {
       return (
         <Centered horizontal vertical>
           <Skeleton />
@@ -130,7 +54,7 @@ class SandboxPage extends React.PureComponent<Props, State> {
       );
     }
 
-    if (this.state.notFound) {
+    if (store.editor.notFound) {
       return (
         <Centered horizontal vertical>
           <Title>
@@ -143,11 +67,11 @@ class SandboxPage extends React.PureComponent<Props, State> {
       );
     }
 
-    if (this.state.error) {
+    if (store.editor.error) {
       return (
         <Centered horizontal vertical>
           <Title>An error occured when fetching the sandbox:</Title>
-          <SubTitle>{this.state.error}</SubTitle>
+          <SubTitle>{store.editor.error}</SubTitle>
           <br />
           <br />
           <Link to="/s/new">Create Sandbox</Link>
@@ -155,7 +79,7 @@ class SandboxPage extends React.PureComponent<Props, State> {
       );
     }
 
-    const sandbox = sandboxes[currentId];
+    const sandbox = store.editor.currentSandbox;
 
     if (sandbox) {
       document.title = sandbox.title
@@ -165,21 +89,13 @@ class SandboxPage extends React.PureComponent<Props, State> {
 
     return (
       <React.Fragment>
-        <Editor
-          match={match}
-          zenMode={zenMode}
-          sandbox={sandbox}
-          workspaceHidden={workspaceHidden}
-          setWorkspaceHidden={viewActions.setWorkspaceHidden}
-        />
-        <KeybindingManager sandboxId={currentId} />
+        <Editor match={match} />
+        {/* <KeybindingManager sandboxId={sandbox.id} /> Use shortcuts provider instead */}
       </React.Fragment>
     );
   }
 }
 
-export default inject('signals')(
-  DragDropContext(HTML5Backend)(
-    connect(mapStateToProps, mapDispatchToProps)(SandboxPage)
-  )
+export default inject('signals', 'store')(
+  DragDropContext(HTML5Backend)(observer(SandboxPage))
 );
